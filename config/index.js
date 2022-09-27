5b59b149c6555143bffe5d21a697541b623c4155
alovelives <107634586+alovelives@users.noreply.github.com>
2022-09-27 12:20:48 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ export const config = {
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
    * ]
-   */
+   */ 
   USERS: [
     {
       // 想要发送的人的名字
@@ -32,8 +32,7 @@ export const config = {
       id: "oVRlR563yNWSmxHX2Yai8RRrR_cM",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "nuCcp5aKj0MhrIrjSaQi6LHT9Va7_VZNWyVMVmaXC1U",
-      id: "oVRlR5xcSRn8Vpjpt_WuuHBJNh08",
-      useTemplateId: "nuCcp5aKj0MhrIrjSaQi6LHT9Va7_VZNWyVMVmaXC1U",
+
       // 所在省份
       province: "湖南",
       // 所在城市
@@ -43,10 +42,10 @@ export const config = {
     }, 
     {
       name: "老婆1", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
+      id: "oVRlR5xcSRn8Vpjpt_WuuHBJNh08",
+      useTemplateId: "nuCcp5aKj0MhrIrjSaQi6LHT9Va7_VZNWyVMVmaXC1U",
+      province: "湖南",
+      city: "长沙",
       openUrl: "https://wangxinleo.cn"
     }, 
     {
